769779de3a473b284f07be8e7f2e268082fae80d
Cristina Lester <cristina.deelester@gmail.com>
2021-09-10 16:31:14 -0700

    added business logic for all CRUD operations

--- app.js
+++ app.js
@@ -6,16 +6,11 @@
 
 const express = require("express"),
   morgan = require("morgan"),
-  uuid = require("uuid"),
   mongoose = require("mongoose"),
   Models = require("./models/models.js");
-const { update } = require("lodash");
-//const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 const server = express();
-//not using body-parser because it's deprecated
-// server.use(bodyParser.json());
-// server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(morgan("common"));
 
@@ -37,6 +32,33 @@ mongoose.connect("mongodb://localhost:27017/NotFlixDB", {
  */
 const PORT = 8080;
 
+function errorMsg(status, err) {
+  let msg = "";
+  switch (status) {
+    case 500:
+      msg = `
+      Oh no! Something went wrong! 😱
+      Error: ${err}
+      `;
+      break;
+    case 404:
+      msg = `
+      I found nothing, sorry! 🦗
+      `;
+    case 400:
+      msg = `
+      Bad Request! 👎
+      Error: ${err}
+      `;
+      break;
+    default:
+      msg = `
+      Something bad happened 🙀!
+      Error: ${err}
+      `;
+  }
+  return msg;
+}
 //Start Business Logic
 
 /**
@@ -45,7 +67,7 @@ const PORT = 8080;
 server.use("/docs", express.static("public/docs"));
 
 /**
- * Main entry point to reach the home page of NotFlix
+ * Main endpoit to reach the home page of NotFlix
  */
 server.get("/", (req, res) => {
   let responseText =
@@ -53,45 +75,213 @@ server.get("/", (req, res) => {
   res.status(200).send(responseText);
 });
 
-server.get("/movies", (req, res) => {
-  Movies.find().then((movies) => {
+//General search
+
+server.get("/movies", async (req, res) => {
+  try {
+    const movies = await Movies.find().populate("Cast", "Name");
     res.status(200).json(movies);
-  });
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
 });
 
-server.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(`
-      Oh no! Something went wrong! 🙀
-      Error: ${err}
-      `);
-    });
+server.get("/users", async (req, res) => {
+  try {
+    const users = await Users.find()
+      .populate("FavoriteMovies", "Title")
+      .populate("WatchList", "Title");
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
 });
 
-server.get("/movies/:movieTitle", (req, res) => {
+server.get("/genres", async (req, res) => {
+  try {
+    const genres = await Genres.find();
+    res.status(200).json(genres);
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/directors", async (req, res) => {
+  try {
+    const directors = await Directors.find().populate("Filmography", "Title");
+    res.status(200).json(directors);
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/actors", async (req, res) => {
+  try {
+    const actors = await Actors.find().populate("Filmography", "Title");
+    res.status(200).json(actors);
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+//Detailed search
+
+server.get("/movies/:movieTitle", async (req, res) => {
   const { movieTitle } = req.params;
-  Movies.findOne({ Title: movieTitle })
-    .then((movie) => {
-      if (movie) {
-        res.status(200).json(movie);
-      } else {
-        res
-          .status(404)
-          .send("Sorry, we don't have that movie in our database! 🤷");
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(`
-      Oh no! Something went wrong! 🙀
-      Error: ${err}
-      `);
+  try {
+    const movie = await Movies.findOne({ Title: movieTitle }).populate(
+      "Cast",
+      "Name"
+    );
+    if (movie) {
+      res.status(200).json(movie);
+    } else {
+      res
+        .status(404)
+        .send("Sorry, we don't have that movie in our database! 🤷");
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/users/:username", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await Users.findOne({ Username: username })
+      .populate("FavoriteMovies", "Title")
+      .populate("WatchList", "Title");
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send(`Sorry, the user ${username} doesn't exist! 🤷`);
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/genres/:name", async (req, res) => {
+  const { name } = req.params;
+  try {
+    const genre = await Genres.findOne({ Name: name });
+    if (genre) {
+      res.status(200).json(genre);
+    } else {
+      res
+        .status(404)
+        .send(`Sorry, we don't have the genre ${genre} in our database! 🤷`);
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/directors/:name", async (req, res) => {
+  const { name } = req.params;
+  try {
+    const director = await Directors.findOne({ Name: name }).populate(
+      "Filmography",
+      "Title"
+    );
+    if (director) {
+      res.status(200).json(director);
+    } else {
+      res
+        .status(404)
+        .send(`Sorry, we don't have the director ${name} in our database! 🤷`);
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/actors/:name", async (req, res) => {
+  const { name } = req.params;
+  try {
+    const actor = await Actors.findOne({ Name: name }).populate(
+      "Filmography",
+      "Title"
+    );
+    if (actor) {
+      res.status(200).json(actor);
+    } else {
+      res
+        .status(404)
+        .send(`Sorry, we don't have the actor ${name} in our database! 🤷`);
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/movies/:movieTitle/cast", async (req, res) => {
+  const { movieTitle } = req.params;
+  try {
+    const movie = await Movies.findOne({ Title: movieTitle }).populate({
+      path: "Cast",
+      select: "Name -_id",
     });
+    if (!movie) {
+      res
+        .status(404)
+        .send(
+          `Sorry, we don't have the movie "${movieTitle}" in our database! 🤷`
+        );
+    } else {
+      res.status(200).json({
+        Cast: movie.Cast,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/users/:username/favorites", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await Users.findOne({ Username: username }).populate({
+      path: "FavoriteMovies",
+      select: "Title -_id",
+    });
+    if (!user) {
+      res
+        .status(404)
+        .send(
+          `Sorry, we don't have the user "${username}" in our database! 🤷`
+        );
+    } else {
+      res.status(200).json({
+        Favorites: user.FavoriteMovies,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
+});
+
+server.get("/users/:username/watchlist", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await Users.findOne({ Username: username }).populate({
+      path: "WatchList",
+      select: "Title -_id",
+    });
+    if (!user) {
+      res
+        .status(404)
+        .send(
+          `Sorry, we don't have the user "${username}" in our database! 🤷`
+        );
+    } else {
+      res.status(200).json({
+        WatchList: user.WatchList,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(errorMsg(500, err));
+  }
 });
 
 server.post("/register", (req, res) => {
@@ -118,21 +308,15 @@ server.post("/register", (req, res) => {
           .then((user) => {
             res.status(201).json(user);
           })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send(`
-              Oh no! Something went wrong! 🙀
-              Error: ${err}
-              `);
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send(errorMsg(500, err));
           });
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send(`
-        Everything broke, it's your fault! 😫 JK, it's not.
-        This is what actually happened: ${err}
-        `);
+      res.status(500).send(errorMsg(500, err));
     });
 });
 
@@ -163,10 +347,7 @@ server.put("/users/:username", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send(`
-          Oh no! Something went wrong! 🙀
-          Error: ${err}
-        `);
+      res.status(500).send(errorMsg(500, err));
     });
 });
 
@@ -186,15 +367,37 @@ server.post("/users/:username/favorites/:movieId", (req, res) => {
       res
         .status(200)
         .send(
-          `The movie has been successfully added to ${username}'s list of favorites!`
+          `The movie has been successfully added to ${username}'s list of favorites! 👌`
         );
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send(`
-      Something happened and it's not a good thing. 😱
-      Error: ${err}
-      `);
+      res.status(500).send(errorMsg(500, err));
+    });
+});
+
+//add movie to user's watchlist
+server.post("/users/:username/watchlist/:movieId", (req, res) => {
+  const { username, movieId } = req.params;
+  Users.findOneAndUpdate(
+    { Username: username },
+    {
+      $addToSet: {
+        WatchList: movieId,
+      },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      res
+        .status(200)
+        .send(
+          `The movie has been successfully added to ${username}'s watch list! 👌`
+        );
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(errorMsg(500, err));
     });
 });
 
@@ -219,10 +422,31 @@ server.delete("/users/:username/favorites/:movieId", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send(`
-      Something happened and it's not a good thing. 😱
-      Error: ${err}
-      `);
+      res.status(500).send(errorMsg(500, err));
+    });
+});
+
+server.delete("/users/:username/watchlist/:movieId", (req, res) => {
+  const { username, movieId } = req.params;
+  Users.findOneAndUpdate(
+    { Username: username },
+    {
+      $pull: {
+        WatchList: movieId,
+      },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      res
+        .status(200)
+        .send(
+          `The movie has been successfully removed from ${username}'s watch list! 👌`
+        );
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(errorMsg(500, err));
     });
 });
 
@@ -240,10 +464,7 @@ server.delete("/users/:username/deregister", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send(`
-      Something happened and it's not a good thing. 😱
-      Error: ${err}
-      `);
+      res.status(500).send(errorMsg(500, err));
     });
 });
 
@@ -254,7 +475,7 @@ server.delete("/users/:username/deregister", (req, res) => {
  */
 server.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Genreal Server Error: Something went wrong 😿");
+  res.status(500).send(errorMsg(500, err));
 });
 
 /**
