bc2567e120719784152e0bbb57ef0df77cdcbde3
Cristina Lester <cristina.deelester@gmail.com>
2021-09-14 17:44:22 -0700

    changed cors to allow all domains

--- app.js
+++ app.js
@@ -21,13 +21,14 @@ const PORT = process.env.PORT || 8080;
 
 //Database Connection
 
-/*
 //Local Database
+/*
 mongoose.connect("mongodb://localhost:27017/NotFlixDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 */
+
 //Atlas Database
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
@@ -41,25 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //CORS integration
-let allowedOrigins = [
-  "http://localhost:8080",
-  "https://michiyoyo.github.io/notflix",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 //Authentication
 let auth = require("./authentication/auth")(app);
