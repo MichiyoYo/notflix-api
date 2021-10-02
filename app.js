2f52ced6ea6e1559b1ef72b9c28746bca1ef4075
Cristina Lester <cristina.deelester@gmail.com>
2021-10-01 22:23:39 -0700

    added cors rules

--- app.js
+++ app.js
@@ -43,6 +43,14 @@ app.use(express.urlencoded({ extended: false }));
 
 //CORS integration
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //Authentication
 let auth = require("./authentication/auth")(app);
