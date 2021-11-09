4eb7daec67442bc59407e6a047e2c65ea1e23cff
Cristina Lester <cristina.deelester@gmail.com>
2021-11-09 09:28:32 +0100

    added expose headers

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
